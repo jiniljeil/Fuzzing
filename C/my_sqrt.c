e0a25ac2a88094be99f29b17a42390862919070c
김진일 <jiniljeil1@naver.com>
2021-08-02 20:47:59 +0900

    fuzzing

--- C/my_sqrt.c
+++ C/my_sqrt.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h> 
+
 #define EPSILON 1e-8
 
 double my_sqrt(double x) {
@@ -27,7 +29,7 @@ double my_sqrt_with_log(double x) {
 }
 
 void assertEquals(double x,double y) {
-    assert(abs(x-y) < EPSILON);
+    assert(fabs(x-y) < EPSILON);
 }
 
 double my_sqrt_checked(double x) { 
@@ -50,22 +52,74 @@ void sqrt_program(char* arg) {
     }
 }
 
-int* shellsort(int *elems, int size) {
-    int * sorted_elems = (int*)malloc(sizeof(int) * size); 
+double my_sqrt_fixed(int x) {
+    assert(0 <= x); 
+    if(x == 0) {
+        return 0 ; 
+    }
+    return my_sqrt(x) ;
+}
+
+
+int* shellsort(int *elems, int size){
+    int elems_size = size; 
+    int * sorted_elems = (int*)malloc(sizeof(int) * elems_size); 
+    for(int i = 0 ; i < size ; i++) sorted_elems[i] = elems[i];
+
     int gaps[8] = {701, 301, 132, 57, 23, 10, 4, 1}; 
     for(int i = 0 ; i < 8; i++) {
-        for(int j = gaps[i]; j < size; j++) {
-            int tmp = sorted_elems[i]; 
+        int gap = gaps[i]; 
+        for(int j = gap; j < elems_size; j++) {
+            int tmp = sorted_elems[j]; 
             int k = j ;
-            while(k >= gaps[i] && sorted_elems[k - gaps[i]] > tmp) {
-                sorted_elems[k] = sorted_elems[k - gaps[i]]; 
-                k -= gaps[i]; 
+            while(k >= gap && sorted_elems[k - gap] > tmp) {
+                sorted_elems[k] = sorted_elems[k - gap]; 
+                k -= gap; 
             }
             sorted_elems[k] = tmp; 
         }
     }
     return sorted_elems; 
 }
+ 
+int is_sorted(int *elems, int size) {
+    for(int i = 0 ; i < size - 1; i++) if(elems[i] > elems[i+1]) return 0;
+    return 1; 
+}
+
+int is_permutation(int *a, int *b, int a_size, int b_size) {
+    if(a_size == b_size) {
+        int *b_check = (int*)malloc(sizeof(int) * b_size); 
+        memset(b_check, 0, sizeof(int) * b_size); 
+        for(int i = 0 ; i < a_size; i++){
+            for(int j = 0 ; j < b_size; j++) {
+                if(a[i] == b[j] && b_check[j] == 0) {
+                    b_check[j] = 1; 
+                    break; 
+                }
+            }
+        }
+        int ret = 1 ; 
+        for(int i = 0 ; i < b_size; i++) {
+            if(b_check[i] == 0) {
+                ret = 0;
+                break; 
+            }
+        }
+        free(b_check); 
+        return ret; 
+    }else{
+        return 0; 
+    }
+}
+
+double *quadratic_solver(int a, int b, int c) {
+    int q = b * b - 4 * a * c ; // b^2 - 4ac
+    double * sol = (double*)malloc(sizeof(double) * 2) ; 
+    sol[0] = (-b + my_sqrt_fixed(q)) / (2*a); 
+    sol[1] = (-b - my_sqrt_fixed(q)) / (2*a); 
+    return sol; 
+} 
 
 int main(void) {
     int x = 0; 
@@ -84,7 +138,7 @@ int main(void) {
     printf("ASSERT TEST\n"); 
     assert(my_sqrt(4) == 2) ;
     
-    assert(abs(my_sqrt(4) - 2) < EPSILON); 
+    assert(fabs(my_sqrt(4) - 2) < EPSILON); 
 
     // Test pass
     assertEquals(my_sqrt(4), 2); 
@@ -126,13 +180,30 @@ int main(void) {
     sqrt_program("-1");
     sqrt_program("xyzzy");
     
-    int arrsize = 3; 
+   
     int arr[3] = {3, 2, 1}; 
+    int arrsize = 3;  
     int* _shellsort = shellsort(arr, arrsize); 
-
+    
     for(int i = 0 ; i < arrsize; i++) {
         printf("%d ", _shellsort[i]);
     }
+    printf("\n");
 
+    int a[4] = {5, 6, 99, 7}; 
+    printf("First element: %d, length: %lu\n", a[0], sizeof(a)/sizeof(int)); 
+
+    int b[3] = {3, 5, 9}; 
+    printf("Sorted: [3, 5, 9]: %s\n", (is_sorted(b, 3) == 1) ? "True" : "False"); 
+
+    int a_test[3] = {3, 2, 1}; 
+    int b_test[3] = {1, 3, 2}; 
+    printf("Permutation: [3, 2, 1], [1, 3, 2]: %s\n", (is_permutation(a_test,b_test, 3, 3) == 1) ? "True" : "False");
+
+    double *q_solver = quadratic_solver(3, 4, 1); 
+    printf("(%.6lf, %.6lf)", q_solver[0], q_solver[1]); 
+
+    free(_shellsort);
+    free(q_solver) ;
     return 0; 
 }
