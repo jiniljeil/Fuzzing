a4453baa137670818ea3eb8f920760e45ea2ffb4
김진일 <jiniljeil1@naver.com>
2021-08-06 13:02:28 +0900

    fuzzing

---
+++ 2. Lexical Fuzzing/test_src/information_leaks.c
@@ -0,0 +1,80 @@
+#include "../include/fuzzer.h"
+#include <assert.h>
+#define BUFF_SIZE 2048
+
+// C substring function definition
+char* substring(char *str, int start_idx, int end_idx) {
+    int string_length = strlen(str);
+
+    if( string_length < end_idx || string_length < start_idx ) {
+        perror("Out of bound error\n"); 
+        return NULL; 
+    }
+
+    char* sub_str = (char*)malloc(sizeof(char) * ((end_idx - start_idx + 1) + 1)); 
+    int idx = 0; 
+    while(idx <= end_idx) {
+        sub_str[idx] = str[start_idx + idx]; 
+        idx++;
+    }
+
+    sub_str[idx] = '\0';
+
+    return sub_str; 
+}
+
+char* make_secrets_string() {
+    char* secrets = (char*)malloc(sizeof(char) * BUFF_SIZE); 
+
+    sprintf(secrets, "<space-for-reply>%s<secret-certificate>%s<secret-key>%s<other-secrets>", 
+        fuzzer(BASIC_LENGTH, BASIC_START, BASIC_RANGE), fuzzer(BASIC_LENGTH, BASIC_START, BASIC_RANGE), fuzzer(BASIC_LENGTH, BASIC_START, BASIC_RANGE));
+
+    char uninitialized_memory_marker[9] = "deadbeef";
+    int uninit_memorymaker_size = strlen(uninitialized_memory_marker) ; 
+
+    while(strlen(secrets) + uninit_memorymaker_size + 1 < 2048) {
+        strcat(secrets, uninitialized_memory_marker); 
+    }
+    return secrets; 
+} 
+
+char* heartbeat(char* reply, int length, char* memory) {
+    char * c = substring(memory, strlen(reply), strlen(memory));
+    sprintf(memory, "%s%s", reply, c);
+
+    char* s = (char*)malloc(sizeof(char) * (length + 1));
+    for(int i = 0 ; i < length ; i++) {
+        s[i] = memory[i];
+    }
+    s[length] = '\0';
+    return s ;
+}
+
+void information_leaks_test1() {
+    char* secrets = make_secrets_string(); 
+
+    printf("[Information Leaks Test1]\n");
+    printf("%s\n", heartbeat("potato", 6, secrets)); 
+    printf("%s\n", heartbeat("bird", 4, secrets));
+    printf("%s\n\n", heartbeat("hat", 500, secrets)); 
+}
+
+void information_leaks_test2() {
+    printf("[Information Leaks Test2]\n");
+    char* secrets = make_secrets_string(); 
+    char uninitialized_memory_marker[9] = "deadbeef";
+
+    for(int i = 0 ; i < 10; i++) {
+        char* s = heartbeat(fuzzer(BASIC_LENGTH, BASIC_START, BASIC_RANGE), 1 + rand()%500, secrets);
+        assert(strstr(s, uninitialized_memory_marker) != NULL);
+        assert(strstr(s, "secret") != NULL);
+    }
+}
+
+int main(void) {
+    srand(time(0)); 
+    information_leaks_test1(); 
+    information_leaks_test2();
+    return 0;
+}
+
---
+++ 2. Lexical Fuzzing/test_src/program.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h> 
+
+/* 
+clang -fsanitize=address -g -o program program.c
+*/
+
+int main(int argc, char** argv) {
+    /* Create an array with 100 bytes, initialized with 42 */
+    char *buf = malloc(100);
+    memset(buf, 42, 100);
+
+    /* Read the N-th element, with N being the first command-line argument */
+    int index = atoi(argv[1]);
+
+    char *buff = malloc(100); 
+    memset(buff, 42, 100);
+    char val = buf[index];
+    printf("%c\n", val);
+    /* Clean up memory so we don't leak */
+    free(buf);
+    return val;
+}
