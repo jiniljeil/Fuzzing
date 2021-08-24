62ef995fcad35fb1534fa3c4f0d2e9fc85718d44
김진일 <jiniljeil1@naver.com>
2021-08-24 13:39:52 +0900

    fuzzing

--- 3. Mutation Fuzzing/src/mutating_url.c
+++ 3. Mutation Fuzzing/src/mutating_url.c
@@ -5,7 +5,10 @@
 
 int http_program(char * url) {
     regex_t regex; 
-    regmatch_t regmatch[6] ; 
+    regmatch_t regmatch[7] ; 
+
+    url_t * url_info = (url_t*)malloc(sizeof(url_t)) ; 
+
     // http://www.google.com/search:8080?q=fuzzing&p=f#ddd
     // Pattern: ^(http|https):\/\/([a-zA-Z0-9\.]+)\/?([^\:]*)\:?([0-9]+)\??([a-zA-Z0-9\&\=]+)\#?(.*)
     char * pattern = "^(http|https):\\/\\/([a-zA-Z0-9\\.]+)\\/\?([^\\:]*)\\:\?([0-9]+)\\\?\?([a-zA-Z0-9\\&\\=]+)\\#\?(.*)";
@@ -14,6 +17,13 @@ int http_program(char * url) {
         fprintf(stderr,"This URL is not matched from URL format."); 
         return 0; 
     } 
+
+    if ( regexec(&regex, url, 7, pmatch, 0) != 0) {
+        fprintf(stderr, "Error: regex execute failed!\n"); 
+        return 0; 
+    } 
+
+
 
     return 1; 
 }
